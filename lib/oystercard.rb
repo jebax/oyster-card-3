8fdb07407bfbf4b499227ef3876385f6f1801f3e
muge <muge.hasilci@hotmail.com>
2018-10-08 23:18:40 +0100

    step-6 maximum card limit is enforced

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 
 class Oystercard
   attr_reader :balance
+  MAXIMUM_CARD_LIMIT = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    raise 'Maximum card limit exceeded' if balance + amount > MAXIMUM_CARD_LIMIT
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,7 +14,12 @@ describe Oystercard do
     it 'tops up money to the card' do
       card = Oystercard.new
       card.top_up(5)
-      expect { subject.top_up(10) }.to change { subject.balance }.by(10)
+      expect { subject.top_up(5) }.to change { subject.balance }.by(5)
+    end
+
+    it 'raises and error if maximum card limit exceeded' do
+      card = Oystercard.new
+      expect { subject.top_up(91) }.to raise_error 'Maximum card limit exceeded'
     end
   end
 end
