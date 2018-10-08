9f9be926b993dc272fb7abd64bbbfa3fd903ad6c
muge <muge.hasilci@hotmail.com>
2018-10-08 23:01:51 +0100

    step-5 top-up functionality is enabled

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,4 +8,13 @@ describe Oystercard do
       expect(subject.balance).to eq(0)
     end
   end
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+    it 'tops up money to the card' do
+      card = Oystercard.new
+      card.top_up(5)
+      expect { subject.top_up(10) }.to change { subject.balance }.by(10)
+    end
+  end
 end
